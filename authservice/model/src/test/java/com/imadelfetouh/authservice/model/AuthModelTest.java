92b66992168d80351d12b6a6ee293934d44bc72c
imad <imad.elfetouh@hotmail.com>
2021-03-27 20:13:35 +0100

    authmodel test

---
+++ authservice/model/src/test/java/com/imadelfetouh/authservice/model/AuthModelTest.java
@@ -0,0 +1,17 @@
+package com.imadelfetouh.authservice.model;
+
+import com.imadelfetouh.authservice.model.dto.AuthModel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AuthModelTest {
+
+    @Test
+    public void testConstructor() {
+        AuthModel authModel = new AuthModel(1, "imad", "user.jpg");
+
+        Assertions.assertEquals(1, authModel.getUserId());
+        Assertions.assertEquals("imad", authModel.getUsername());
+        Assertions.assertEquals("imad.jpg", authModel.getPhoto());
+    }
+}
