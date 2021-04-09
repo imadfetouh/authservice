ed479d8c749ec31ccd26a6c053aa791456f2d85d
imad <imad.elfetouh@hotmail.com>
2021-04-09 11:08:54 +0200

    test encpoint

---
+++ authservice/rest/src/main/java/com/imadelfetouh/authservice/rest/endpoints/TestResource.java
@@ -0,0 +1,17 @@
+package com.imadelfetouh.authservice.rest.endpoints;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/test")
+public class TestResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response test() {
+        return Response.status(200).entity("hoi").build();
+    }
+}
