6180c131a68f0b49602cbf90b8a19c38e942adee
imad <imad.elfetouh@hotmail.com>
2021-04-24 12:03:42 +0200

    password hash

--- authservice/dal/src/main/java/com/imadelfetouh/authservice/dal/configuration/SessionReadConfiguration.java
+++ authservice/dal/src/main/java/com/imadelfetouh/authservice/dal/configuration/SessionReadConfiguration.java
@@ -1,22 +1,37 @@
 package com.imadelfetouh.authservice.dal.configuration;
 
+import com.imadelfetouh.authservice.dal.ormmodel.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 
+import java.util.Properties;
+
 public class SessionReadConfiguration {
 
+    private final static SessionReadConfiguration sessionReadConfiguration = new SessionReadConfiguration();
+    private final ReadWriteConfiguration readWriteConfiguration;
     private final SessionFactory sessionFactory;
 
     public SessionReadConfiguration() {
-        Configuration configuration = ReadWriteConfiguration.getInstance().getConfiguration();
+        readWriteConfiguration = ReadWriteConfiguration.getInstance();
+        Configuration configuration = new Configuration();
+
+        Properties properties = readWriteConfiguration.getProperties();
+        configuration.addProperties(properties);
         configuration.getProperties().put(Environment.URL, "jdbc:mysql://"+System.getenv("AUTHSERVICE_MYSQL_REPLICA_HOST")+":"+System.getenv("AUTHSERVICE_MYSQL_REPLICA_PORT")+"/authservice");
+
+        configuration.addAnnotatedClass(User.class);
 
         sessionFactory = configuration.configure().buildSessionFactory();
     }
 
     public Session getSession() {
         return sessionFactory.openSession();
+    }
+
+    public static SessionReadConfiguration getInstance() {
+        return sessionReadConfiguration;
     }
 }
--- authservice/dal/src/main/java/com/imadelfetouh/authservice/dal/signin/SignInDB.java
+++ authservice/dal/src/main/java/com/imadelfetouh/authservice/dal/signin/SignInDB.java
@@ -3,6 +3,7 @@ package com.imadelfetouh.authservice.dal.signin;
 import com.imadelfetouh.authservice.dal.configuration.Executer;
 import com.imadelfetouh.authservice.dal.configuration.SessionType;
 import com.imadelfetouh.authservice.dal.queryexecuter.SignInExecuter;
+import com.imadelfetouh.authservice.dal.security.PasswordHash;
 import com.imadelfetouh.authservice.dalinterface.SignInDal;
 import com.imadelfetouh.authservice.model.jwt.UserData;
 import com.imadelfetouh.authservice.model.response.ResponseModel;
@@ -12,11 +13,13 @@ public class SignInDB implements SignInDal {
     private Executer<UserData> executer;
 
     public SignInDB() {
-        executer = new Executer<>(SessionType.WRITE);
+        executer = new Executer<>(SessionType.READ);
     }
 
     @Override
     public ResponseModel<UserData> signIn(String username, String password) {
-        return executer.execute(new SignInExecuter(username, password));
+        String hashedPassword = PasswordHash.getInstance().hash(password);
+
+        return executer.execute(new SignInExecuter(username, hashedPassword));
     }
 }
